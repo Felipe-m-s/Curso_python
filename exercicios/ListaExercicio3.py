31e32dde4f730389cd4af5570e762e599545ae51
Felipe-m-s <fs33143martins@gmail.com>
2025-10-07 21:56:37 -0300

    Inicio da Lista de Atividade 3

---
+++ exercicios/ListaExercicio3.py
@@ -0,0 +1,48 @@
+
+#todo Questão 1
+for i in range(51):
+    print(i, " ")
+
+#todo Questão 2
+for i in range(100, 0, -1):
+    print(i, " ")
+
+#todo Questão 3
+for i in range(100, 201):
+    print(i, " ")
+
+#todo Questão 4
+for i in range(200, 99, -1):
+    print(i, " ")
+
+#todo Questão 5
+for i in range(0, 501):
+    if i % 5 != 0:
+        continue
+    print(i, " ")
+
+#todo Questão 6
+for i in range(1, 101):
+    if i % 2 == 0:
+        continue
+    print(i, " ")
+
+#todo Questão 7
+
+
+#todo Questão 8
+#todo Questão 9
+#todo Questão 10
+#todo Questão 11
+#todo Questão 12
+#todo Questão 13
+#todo Questão 14
+#todo Questão 15
+#todo Questão 16
+#todo Questão 17
+#todo Questão 18
+#todo Questão 19
+#todo Questão 20
+#todo Questão 21
+#todo Questão 22
+#todo Questão 23
