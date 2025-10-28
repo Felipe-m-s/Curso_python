c83598258f61724ce15be309e0f2bb5c85fdbe21
Felipe-m-s <fs33143martins@gmail.com>
2025-10-28 20:36:43 -0300

    Introdução a funções

---
+++ aulas/AulaFuncao.py
@@ -0,0 +1,17 @@
+def soma(a, b):
+    soma = a + b
+    print(soma)
+
+soma(4, 5)
+soma(8, 9)
+soma(2, 1)
+
+def mensagem():
+    print("Minha segunda função")
+
+mensagem()
+
+def saudacao(nome, idade):
+    print(f"Olá, {nome}!\nVocê tem {idade} anos!")
+
+saudacao("Felipe", 19)
