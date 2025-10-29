fec3c4c1d46fc34dcdfa40ac7315500081b15f7e
Felipe-m-s <fs33143martins@gmail.com>
2025-10-28 21:41:27 -0300

    Continuação das aulas sobre função

--- aulas/AulaFuncao.py
+++ aulas/AulaFuncao.py
@@ -15,3 +15,29 @@ def saudacao(nome, idade):
     print(f"Olá, {nome}!\nVocê tem {idade} anos!")
 
 saudacao("Felipe", 19)
+
+def soma_com_return (a, b):
+    return a + b
+
+x = soma_com_return(5, 7)
+print("Resultado:", x)
+
+def fatorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fatorial(n-1)
+
+print(fatorial(5))
+
+def quadrado(n_parm):
+    if n_parm == 1:
+        return 1
+    else:
+        return pow(n_parm, 2)
+
+print(f"O quadrado de 1 é {quadrado(5)}")
+for i in range(1, 6):
+    resultado = quadrado(i)
+    print(f"O quadrado de {i} é {quadrado(i)}")
+    
