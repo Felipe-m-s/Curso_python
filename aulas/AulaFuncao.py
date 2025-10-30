e59514a97816e903a26505c78c2007dbb1f573df
Felipe-m-s <fs33143martins@gmail.com>
2025-10-30 19:53:42 -0300

    Continuação da aula sobre funções e resolução de uma atividade de fixação

--- aulas/AulaFuncao.py
+++ aulas/AulaFuncao.py
@@ -40,4 +40,62 @@ print(f"O quadrado de 1 é {quadrado(5)}")
 for i in range(1, 6):
     resultado = quadrado(i)
     print(f"O quadrado de {i} é {quadrado(i)}")
+
+
+
+#* ----------------- Função com empacotamento (Quando eu não sei quanto paramentos a função vai receber) -----------------
+def contador (*num_parm):
+    tam = len(num_parm)
+    print(f"Foram recebidos os valores {num_parm} e são, ao todo, {tam} números")
+
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6, 2)
+
+def somar (*args):
+    print(f"Recebi os valores: ", args)
+    print ("A soma é: ", sum(args))
+
+somar(2, 4, 6)
+somar(10, 20)
+somar(10, 20, 30, 40, 50)
+
+
+#* ----------------- Função com empacotamento, com agumentos nomeados (Quando eu não sei quanto paramentos a função vai receber) -----------------
+def dados (**kwargs):
+    print("Dados recebidos")
+    for chave, valor in kwargs.items():
+        print(f"{chave} : {valor}")
+
+dados(nome = "Ana", idade = 25, cidade = "SJE")
+dados(produto = "Notebook", preco = 3500)
+
+#todo Crie uma função chamada "exibeMenu": 0 - sair; 1 - somar.
+#todo A função deve ler a opção do usuario e retornar um valor inteiro
+
+def exibeMenu ():
     
+    print("-------- MENU --------")
+    print("0 - Sair\n1 - Somar")
+        
+    opcao = int(input("Digite a opção desejada: "))
+    return opcao
+
+
+#todo Função para somar dois numeros
+def somarDoisNum(num1_parm, num2_parm):
+    return num1_parm + num2_parm
+
+#todo Implementando o programa principal
+while True:
+    match exibeMenu():
+        case 0:
+            break
+        case 1:
+            num1 = int(input("Digite o primeiro número: "))
+            num2 = int(input("Digite o segundo número: "))
+            print(f"A soma de {num1} + {num2} é igual à {somarDoisNum(num1, num2)}")
+            continue
+        case _:
+            print("Opção invalida")
+            continue
