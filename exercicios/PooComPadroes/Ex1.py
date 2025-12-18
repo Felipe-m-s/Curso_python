90aa42359985f2a949abad5a6c1d2fbce39330ce
Felipe-m-s <fs33143martins@gmail.com>
2025-12-18 20:41:46 -0300

    Finalizando exercicio 1

--- exercicios/PooComPadroes/Ex1.py
+++ exercicios/PooComPadroes/Ex1.py
@@ -32,5 +32,83 @@ class NotificacaoFactory:
         else:
             raise ValueError("Tipo de notificação invalida.")
 
-#todo Uso do Factory Method
+#! Decorator
+class NotificacaoDecorator(Notificacao):
+    def __init__(self, notificacao):
+        self.notificacao = notificacao
+    
+    def enviar(self, mensagem):
+        self.notificacao.enviar(mensagem)
 
+class CriptografiaDecorator(NotificacaoDecorator):
+    def enviar(self, mensagem):
+        mensagem_criptografada = f"[CRIPTO] {mensagem}"
+        self.notificacao.enviar(mensagem_criptografada)
+
+class LogDecorator(NotificacaoDecorator):
+    def enviar (self, mensagem):
+        print(f"[LOG] enviando")
+        self.notificacao.enviar(mensagem)
+        print("[LOG] enviado com sucesso")
+
+#! Observer
+class Usuario:
+    def __init__(self, nome):
+        self.nome = nome
+    
+    def receberNotificacao(self, mensagem):
+        print(f"{self.nome} recebeu {mensagem}")
+
+class SistemaNotificacao:
+    def __init__(self):
+        self.usuarios = []
+        
+    def adicionarUsuario(self, usuario):
+        self.usuarios.append(usuario)
+        print(f"{usuario.nome} adicionado ao sistema de notificações.")
+    
+    def notificarUsuarios(self, mensagem):
+        print(f"Notificando {len(self.usuarios)} usuários...")
+        for usuario in self.usuarios:
+            usuario.receberNotificacao(mensagem)
+
+#Main
+def main():
+    print("="*50)
+    print("Sistema de Notificações")
+    print("="*50)
+    
+    # Criando sistema de notificações
+    sistema = SistemaNotificacao()
+    
+    # Cadastrando usuários
+    sistema.adicionarUsuario(Usuario("Alice"))
+    sistema.adicionarUsuario(Usuario("Bob"))
+    sistema.adicionarUsuario(Usuario("Charlie"))
+    
+    # Criando notificações usando a fábrica
+    email = NotificacaoFactory.criarNotificacao("email")
+    sms = NotificacaoFactory.criarNotificacao("sms")
+    
+    email.enviar("Bem-vindo!")
+    sms.enviar("Seu código é 1234.")
+    
+    # Funcionalidades adicionais com Decorator
+    print("\nUsando Decorators:")
+    email_log = LogDecorator(email)
+    sms_cripto = CriptografiaDecorator(sms)
+    
+    email_log.enviar("Notificação importante por email.")
+    sms_cripto.enviar("Dados sensíveis por SMS.")
+    
+    # Notificando todos os usuários
+    print("\nNotificando todos os usuários:")
+    sistema.notificarUsuarios("Sistema em manutenção hoje às 22h.")
+    
+    # Juntando todos os padrões
+    print("\nUsando todos os padrões juntos:")
+    notificacaoAvancada = LogDecorator(CriptografiaDecorator(NotificacaoFactory.criarNotificacao("email")))
+    notificacaoAvancada.enviar("Informações para deposito bancário.")
+    
+
+main()
